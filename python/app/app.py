cacb8c6e341ae65aa88b1f876d26e355ea1c777c
ricardoher12 <ricardo.hernandezr2@hotmail.com>
2020-03-25 19:16:01 -0600

    first verion of config.yml

---
+++ python/app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+times = 0
+
+
+@app.route('/')
+def hello():
+    global times
+    times += 1
+    return 'Hello this has been viewed  time(s). ' + str(times)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
